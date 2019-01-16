5d86bf7d205706596a9e6f16bf2c01eea1f4174b
stevenwil <steven@misfit-inc.com>
2019-01-17 00:06:34 +0800

    body sections

--- index.php
+++ index.php
@@ -13,29 +13,29 @@
 			<div class="header-nav__pagination-container">
 				<div>
 					<div class="header-nav__hor-line"></div>
-					<div class="header-nav__item">
-						<div class="header-nav__number-container header-nav__container--active">
+					<div class="header-nav__item header-nav__item--active">
+						<div class="header-nav__number-container">
 							<span>1</span>
 						</div>
 						<label>Flights</label>
 					</div>
-					<div class="header-nav__item">
+					<div class="header-nav__item header-nav__item--two">
 						<div class="header-nav__number-container">
 							<span>2</span>
 						</div>
-						<label>Flights</label>
+						<label>Passengers</label>
 					</div>
-					<div class="header-nav__item">
+					<div class="header-nav__item header-nav__item--three">
 						<div class="header-nav__number-container">
 							<span>3</span>
 						</div>
-						<label>Flights</label>
+						<label>Ads-ons</label>
 					</div>
-					<div class="header-nav__item">
+					<div class="header-nav__item header-nav__item--fourth">
 						<div class="header-nav__number-container">
 							<span>4</span>
 						</div>
-						<label>Flights</label>
+						<label>Payment</label>
 					</div>
 				</div>
 			</div>
@@ -46,7 +46,132 @@
 				<span>&#183; 2 Adults, 1 Child</span>
 			</div>
 		</div>
+		<div class="flight-info">
+			<div class="flight-info__item">
+				<div class="flight-info__single_container">
+					<h2>Departing</h2>
+					<div>
+						<p class="day">12 may (fri)</p>
+						<p class="time">sin 09:20</p>
+						<p class="location">Singapore</p>
+					</div>
+					<div>
+						<img src="images/plane.png">
+					</div>
+					<div>
+						<p class="day">12 may (fri)</p>
+						<p class="time">sfo 09:40</p>
+						<p class="location">San Francisco</p>
+					</div>
+				</div>
+			</div>
+			<div class="flight-info__item">
+				<div class="flight-info__single_container">
+					<h2>Returning</h2>
+					<div>
+						<p class="day">20 may (sat)</p>
+						<p class="time">sfo 11:30</p>
+						<p class="location">San Francisco</p>
+					</div>
+					<div>
+						<img src="images/plane.png">
+					</div>
+					<div>
+						<p class="day">21 may (sun)</p>
+						<p class="time">sin 19:00</p>
+						<p class="location">Singapore</p>
+					</div>
+				</div>
+			</div>
+			<div class="flight-info__item">
+				<div class="flight-info__single_container">
+					<h2>Total Fare</h2>
+					<div>
+						<p class="day">2 adults, 1 child</p>
+						<p class="time">SGD 888.00</p>
+						<p class="location location--italic">Total fare including taxes and surcharges</p>
 
+					</div>
+					<a href="#"><span>More details</span> <img src="images/down-arrow-link.png"></a>
+				</div>
+			</div>
+		</div>
+		<div class="body-container">
+			<div class="body-container__max-width">
+				<div class="edit-search">
+					<img src="images/upper-design.png">
+					<span>edit search</span>
+				</div>
+				<div class="info-with-image">
+					<img src="images/sitting-img.png">
+					<span>Your travel party enjoys complimentary Standard Seat selection at booking, as you’re travelling with a child</span>
+				</div>
+				<div class="flights-all-info">
+					<h2>1. Singapore to San Francisco</h2>
+					<div class="flights-all-info__container">
+						<h3>Nonstop &#183; 15hr 15mins</h3>
+						<div class="flights-all-info__width-80">
+							<div class="left">
+								<div>
+									<p class="time">sin 09:25</p>
+									<p class="location">Singapore</p>
+									<p class="day">Fri 12 May</p>
+								</div>
+								<div>
+									<img src="images/plane-with-line.png">
+									<label>15h 15m</label>
+								</div>
+								<div>
+									<p class="time">sin 09:25</p>
+									<p class="location">Singapore</p>
+									<p class="day">Fri 12 May</p>
+								</div>
+							</div>
+							<div class="right">
+								<p>Plus Ultra Airlines • SQ 32</p>
+								<a href=""> &rsaquo; More details </a>
+							</div>
+						</div>
+						<div class="flights-all-info__width-20">
+							<p>economy super saver</p>
+							<a href="">Change</a>
+						</div>
+					</div>
+				</div>
+				<div class="flights-all-info">
+					<h2>2. San Francisco to Singapore</h2>
+					<div class="flights-all-info__container">
+						<h3>Nonstop &#183; 16hr 30 mins</h3>
+						<div class="flights-all-info__width-80">
+							<div class="left">
+								<div>
+									<p class="time">SFO 11:30</p>
+									<p class="location">San Francisco</p>
+									<p class="day">Sat 20 May</p>
+								</div>
+								<div>
+									<img src="images/plane-with-line.png">
+									<label>16h 20m</label>
+								</div>
+								<div>
+									<p class="time">SIN 19:00</p>
+									<p class="location">Singapore</p>
+									<p class="day">Sun 21 May</p>
+								</div>
+							</div>
+							<div class="right">
+								<p>Plus Ultra Airlines • SQ 31</p>
+								<a href=""> &rsaquo; More details </a>
+							</div>
+						</div>
+						<div class="flights-all-info__width-20">
+							<p>economy lite</p>
+							<a href="">Change</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 		<script defer type="text/javascript" src="js/scripts.js"></script>
 	</body>
 </html>
