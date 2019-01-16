f620bdc6b8b9eb83089d359e41953c04d5fe6ac7
stevenwil <steven@misfit-inc.com>
2019-01-16 19:59:52 +0800

    header pagination markup

--- index.php
+++ index.php
@@ -9,7 +9,35 @@
 	</head>
 
 	<body>
-		
+		<div class="header-nav">
+		<div class="header-nav__pagination-container">
+				<div>
+					<div class="header-nav__hor-line"></div>
+					<div class="header-nav__item">
+						<div class="header-nav__number-container header-nav__container--active">
+							<span>1</span>
+						</div>
+					</div>
+					<div class="header-nav__item">
+						<div class="header-nav__number-container">
+							<span>2</span>
+						</div>
+					</div>
+					<div class="header-nav__item">
+						<div class="header-nav__number-container">
+							<span>3</span>
+						</div>
+					</div>
+					<div class="header-nav__item">
+						<div class="header-nav__number-container">
+							<span>4</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<p>SIN-SFO SFO-SIN · 2 Adults, 1 Child</p>
+		</div>
+
 		<script defer type="text/javascript" src="js/scripts.js"></script>
 	</body>
 </html>
